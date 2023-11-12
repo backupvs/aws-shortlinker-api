aab449c3d6b98086cce4a87cf8097dc0fb212810
backupvs <dmitryholubev@gmail.com>
2023-11-12 22:53:03 +0200

    Fixed handler path

--- src/lambdas/auth/sign-up/index.ts
+++ src/lambdas/auth/sign-up/index.ts
@@ -2,7 +2,7 @@ import schema from './schema';
 import { handlerPath } from '@libs/handler-resolver';
 
 export default {
-  handler: `${handlerPath(__dirname)}/sign-up.handler.main`,
+  handler: `${handlerPath(__dirname)}/handler.main`,
   events: [
     {
       http: {
