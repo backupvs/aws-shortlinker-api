4aceef7670f8e50a764869e07673a301eb02cf84
backupvs <dmitryholubev@gmail.com>
2023-11-16 14:46:58 +0200

    Fix import paths

--- src/lambdas/index.ts
+++ src/lambdas/index.ts
@@ -2,12 +2,12 @@ export enum Authorizers {
   JweAuthorizer = 'jweAuthorizer',
 }
 
-export { default as jweAuthorizer } from './jwe-authorizer';
+export { default as jweAuthorizer } from './authorizers/jwe-authorizer';
 export { default as signUp } from './auth/sign-up';
 export { default as signIn } from './auth/sign-in';
 export { default as createShortLink } from './short-links/create-short-link';
 export { default as getShortLinks } from './short-links/get-short-links';
 export { default as deactivateShortLink } from './short-links/deactivate-short-link';
 export { default as rootHandler } from './root';
-export { default as deactivateExpiredShortLinks } from './cron/deactivate-expired-short-links';
+export { default as deactivateExpiredShortLinks } from './cron/deactivate-expired-links';
 export { default as sendNotifications } from './send-notifications';
