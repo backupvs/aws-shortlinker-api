e3d6add514eefa304f264d1431854c0bd664077e
backupvs <dmitryholubev@gmail.com>
2023-11-16 21:53:48 +0200

    Removed typo

--- src/common/email-notifications-formatter/handlers.ts
+++ src/common/email-notifications-formatter/handlers.ts
@@ -5,7 +5,7 @@ export const linkExpiredFormatter: MessagesFormatHandler = (params) => {
 
   const contentType = 'html';
   const subject = 'Short link expired';
-  const message = `Short link with ID <b>${shortLinkId}"</b> has been deactivated.<br>
+  const message = `Short link with ID <b>${shortLinkId}</b> has been deactivated.<br>
       Reason: <b>${reason}</b>`;
 
   return { subject, message, contentType };
